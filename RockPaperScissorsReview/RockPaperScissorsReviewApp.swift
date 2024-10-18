25148561dfb483c97618de5a616e2dacf8968eb9
Matt Riley <matthew.riley@barren.kyschools.us>
2024-10-18 08:21:12 -0500

    Initial Commit

---
+++ RockPaperScissorsReview/RockPaperScissorsReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsReviewApp.swift
+//  RockPaperScissorsReview
+//
+//  Created by BCCS 2022 on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
