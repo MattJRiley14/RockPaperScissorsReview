796f305fa6d237adbb5d80fda5f714b9f733fa40
Matt Riley <matthew.riley@barren.kyschools.us>
2024-10-22 08:30:53 -0500

    Added extra features (Win/Lose, Score, Number of questions)

--- RockPaperScissorsReview/ContentView.swift
+++ RockPaperScissorsReview/ContentView.swift
@@ -18,6 +18,16 @@ struct ContentView: View {
     // For picking a random option (Rock, Paper, or Scissors)
     @State private var randomIndex = Int.random(in: 0...2)
     
+    // Try to WIN or LOSE feature
+    @State private var tryToWin = true
+    
+    // Keep track of score
+    @State private var score = 0
+    
+    // Keep track of number of questions
+    @State private var questions = 0
+    @State private var showFinalAlert = false
+    
     var body: some View {
         
         VStack {
@@ -29,7 +39,9 @@ struct ContentView: View {
 
             VStack {
                 VStack{
-                    Text("Select the option to WIN")
+                    
+                    // Try to WIN or LOSE feature
+                    Text("Select the option to \(tryToWin ? "WIN" : "LOSE")")
                           .font(.title)
                           .foregroundStyle(.white)
                       
@@ -57,25 +69,87 @@ struct ContentView: View {
             .cornerRadius(25)
 
             Spacer()
+            
+            // Display Score
+            Text("Score: \(score)")
+                .font(.title)
+            
             Spacer()
         }
         .alert(alertMessage, isPresented: $showAlert) {
             Button("Continue", action: reload)
         }
+        
+        // Final alert
+        .alert(alertMessage, isPresented: $showFinalAlert) {
+            Button("Restart Game", action: resetGame)
+        }
+
     }
     
     func optionTapped(_ num: Int) {
-        // Since each option button is assigned a number (Rock = 0, Paper = 1, Scissors = 2) those numbers can be used to determine if the user responed correctly
-        if num - 1 == randomIndex || num + 2 == randomIndex {
-            alertMessage = "Correct"
-        } else {
-            alertMessage = "Wrong"
+        
+        // Update questions variable
+        questions += 1
+        print(questions)
+        
+        // Try to WIN or LOSE feature
+        if tryToWin {
+
+            // Since each option button is assigned a number (Rock = 0, Paper = 1, Scissors = 2) those numbers can be used to determine if the user responed correctly
+            if num - 1 == randomIndex || num + 2 == randomIndex {
+                alertMessage = "Correct"
+                
+                // Add to score
+                score += 1
+            } else {
+                alertMessage = "Wrong"
+                
+                // Subtract from score
+                if score > 0 {
+                    score -= 1
+                }
+            }
+
+        } else  {
+            
+            // Try to WIN or LOSE feature
+            if num + 1 == randomIndex || num - 2 == randomIndex {
+                alertMessage = "Correct"
+
+                // Add to score
+                score += 1
+            } else {
+                alertMessage = "Wrong"
+
+                // Subtract from score
+                if score > 0 {
+                    score -= 1
+                }
+            }
         }
-        showAlert = true
+        
+        // Checking to see if game is over (AKA question equals 10)
+        if questions == 10 {
+            alertMessage = "Your final score is \(score)"
+            showFinalAlert = true
+        } else  {
+            showAlert = true
+        }
     }
     
     func reload() {
         randomIndex = Int.random(in: 0...2)
+        
+        // Try to WIN or LOSE feature
+        tryToWin = !tryToWin
+    }
+    
+    // Function for reseting the game
+    func resetGame(){
+        reload()
+        questions = 0
+        score = 0
     }
 }
 
